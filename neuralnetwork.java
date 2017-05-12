e24a884554f4cdd7cd5f2d1e1c7f8046eec3400c
Reece Martin <capt.rm.fae@gmail.com>
2017-05-11 22:28:10 -0700

    Update neuralnetwork.java
    Adds a function for calculating the value of the output for the Neural Network based on two inputs. 
    Adds variables for Neuron and Weight Values.
    Adds a TBC activation function.

--- neuralnetwork.java
+++ neuralnetwork.java
@@ -6,27 +6,91 @@ import java.util.Scanner;
 
 public class neuralnetwork{
 
-    //Variables for correct (trained) input and output values
+    // Variables for correct (trained) input and output values
     private double[] input1c;
     private double[] input2c;
     private double[] outputc;
 
+    // Variables for neuron values
+
+    // Inputs are limited to the range of zero to one inclusive
+    private double input1;
+    private double input2;
+
+    private double hiddenLayerA1Neuron;
+    private double hiddenLayerA2Neuron;
+    private double hiddenLayerB1Neuron;
+    private double hiddenLayerB2Neuron;
+    private double bias1 = 1;
+    private double bias2 = 1;
+    private double bias3 = 1;
+
+    // Weights (layer type-layer-neuron number-weight)
+    //Weights for Hidden Layer A Neuron 1
+    private double ha1a;
+    private double ha1b;
+    private double ha1bias;
+
+    //Weights for Hidden Layer A Neuron 2
+    private double ha2a;
+    private double ha2b;
+    private double ha2bias;
+
+    //Weights for Hidden Layer B Neuron 1
+    private double hb1a;
+    private double hb1b;
+    private double hb1bias;
+
+    //Weights for Hidden Layer B Neuron 2
+    private double hb2a;
+    private double hb2b;
+    private double hb2bias;
+
+    //Weights for Output Neuron
+    private double o1a;
+    private double o1b;
+    private double o1bias;
 
     public neuralnetwork(){
+
+        // Ideal Values (To Be Used in Training)
         input1c = new double[4];
         input2c = new double[4];
         outputc = new double[4];
+
+
+
     }
 
     /*
-    public void trainNetwork{
-
-    }
-
-    public void testNetwork{
+    public void trainNetwork(){
 
     }
     */
+
+    public double testNetwork(double inputf1, double inputf2){
+
+        //Calculation for first layer of hidden neurons
+        double hiddenLayerA1 = activationFunction(inputf1 * ha1a * inputf2 * ha1b * bias1 * ha1bias);
+        double hiddenLayerA2 = activationFunction(inputf1 * ha2a * inputf2 * ha2b * bias1 * ha2bias);
+
+        //Calculation for second layer of hidden neurons
+        double hiddenLayerB1 = activationFunction(hiddenLayerA1 * hb1a * hiddenLayerA2 * hb1b * bias2 * hb1bias);
+        double hiddenLayerB2 = activationFunction(hiddenLayerA1 * hb2a * hiddenLayerA2 * hb2b * bias2 * hb2bias);
+
+        //Calculation for output
+        return activationFunction(hiddenLayerB1);
+
+    }
+
+    /*
+    public static double activationFunction(double value){
+
+    }
+    */
+
+    // Method used for user to input data to be trained to the neural network - data is entered in the form of a 3 X 4
+    // truth table and so only binary operator emulation can be achieved with this neural network implementation
 
     public void inputTrainingData(){
         Scanner read = new Scanner(System.in);
@@ -41,7 +105,11 @@ public class neuralnetwork{
     }
 
     public static void main(String[] args) {
+
+        // Initializes a new neural network
         neuralnetwork net = new neuralnetwork();
+
+        // Calls method to prompt user to input data to be trained to the neural network
         net.inputTrainingData();
     }
 }
